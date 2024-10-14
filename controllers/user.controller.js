c96049dd8a23e511adf3fc45b0d385f4782ff9a7
hemel17 <redewanmahmud@gmail.com>
2024-10-14 20:52:48 +0600

    update user added

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -7,11 +7,15 @@ const createUser = asyncHandler(async (req, res) => {
   const email = req.body.email;
   const findUser = await User.findOne({ email });
 
-  if (!findUser) {
-    const newUser = await User.create(req.body);
-    res.status(200).json(newUser);
-  } else {
-    throw new Error("User already exists!");
+  try {
+    if (!findUser) {
+      const newUser = await User.create(req.body);
+      res.status(200).json(newUser);
+    } else {
+      throw new Error("User already exists!");
+    }
+  } catch (error) {
+    throw new Error(error);
   }
 });
 
@@ -20,35 +24,77 @@ const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
   const findUser = await User.findOne({ email });
 
-  if (findUser && (await findUser.isPasswordMatched(password))) {
-    res.status(200).json({
-      name: findUser?.name,
-      email: findUser?.email,
-      token: generateToken(findUser?._id),
-    });
-  } else {
-    throw new Error("Invalid credentials!");
+  try {
+    if (findUser && (await findUser.isPasswordMatched(password))) {
+      res.status(200).json({
+        name: findUser?.name,
+        email: findUser?.email,
+        token: generateToken(findUser?._id),
+      });
+    } else {
+      throw new Error("Invalid credentials!");
+    }
+  } catch (error) {
+    throw new Error(error);
   }
 });
 
 // * get all users
 const getAllUsers = asyncHandler(async (req, res) => {
-  const users = await User.find();
-  res.status(200).json(users);
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    throw new Error(error);
+  }
 });
 
 // * get one user
 const getOneUser = asyncHandler(async (req, res) => {
-  const { id } = req.params;
-  const user = await User.findById(id);
-  res.status(200).json(user);
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    res.status(200).json(user);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+// * update user
+const updateUser = asyncHandler(async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedUser = await User.findByIdAndUpdate(
+      id,
+      {
+        name: req?.body.name,
+        email: req?.body.email,
+      },
+      {
+        new: true,
+      }
+    );
+    res.send(updatedUser);
+  } catch (error) {
+    throw new Error(error);
+  }
 });
 
 // * delete user
 const deleteUser = asyncHandler(async (req, res) => {
-  const { id } = req.params;
-  const deletedUser = await User.findByIdAndDelete(id);
-  res.status(200).json(deletedUser);
+  try {
+    const { id } = req.params;
+    const deletedUser = await User.findByIdAndDelete(id);
+    res.status(200).json(deletedUser);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+// * delete all user
+const deleteAllUser = asyncHandler(async (req, res) => {
+  const deleted = await User.deleteMany();
+  res.send(deleted);
 });
 
 module.exports = {
@@ -56,5 +102,7 @@ module.exports = {
   loginUser,
   getAllUsers,
   getOneUser,
+  updateUser,
   deleteUser,
+  deleteAllUser,
 };
--- routes/user.route.js
+++ routes/user.route.js
@@ -5,12 +5,14 @@ const {
   loginUser,
   getOneUser,
   deleteUser,
+  updateUser,
+  deleteAllUser,
 } = require("../controllers/user.controller");
 const router = express.Router();
 
-router.route("/").get(getAllUsers);
+router.route("/").get(getAllUsers).delete(deleteAllUser);
 router.route("/register").post(createUser);
 router.route("/login").post(loginUser);
-router.route("/:id").get(getOneUser).delete(deleteUser);
+router.route("/:id").get(getOneUser).put(updateUser).delete(deleteUser);
 
 module.exports = router;
